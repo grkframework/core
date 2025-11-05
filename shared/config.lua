22813d046d661e2b5c42a642f4d8a02ffc626bfd
Grk Framework <grkframework@gmail.com>
2025-11-05 13:26:50 -0300

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,26 @@
+Grk = {}
+
+-- Config Geral
+Grk.Version = "1.0.0"
+Grk.Debug = true
+
+-- Banco de Dados
+Grk.DB = {
+    name = "fivem_grk",
+    host = "127.0.0.1",
+    user = "root",
+    pass = "grk123"
+}
+
+-- IA Settings (Grok API)
+Grk.AI = {
+    enabled = false,
+    api_key = "SUA_CHAVE_GROK_AQUI",
+    model = "grok-beta"
+}
+
+-- HUD
+Grk.HUD = {
+    enabled = true,
+    position = "top-right"
+}
